ea2109516590609249ddcf0e1a88d8935eead9b9
carlchizhang <16144434+carlchizhang@users.noreply.github.com>
2018-04-30 23:43:26 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function PowerDisplay(props) {
+  return (
+    <div>
+      <h1>Computing Power: {props.curComputingPower}</h1>
+      <button className="increment-power">
+        Increase
+      </button>
+    </div>
+  );
+}
+
+class Navbar extends Component {
+  render() {
+    return (
+      <header className="navbar">
+        <PowerDisplay
+          curComputingPower={1000}
+        />
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar/>
       </div>
     );
   }
